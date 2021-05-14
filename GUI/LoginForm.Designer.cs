e0bd9078d0530f6f5e29fd554af1f43e8a93b91b
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2021-05-14 12:43:51 -0700

    agrego imagenes

--- GUI/LoginForm.Designer.cs
+++ GUI/LoginForm.Designer.cs
@@ -29,13 +29,80 @@ namespace Corvus_Proyecto
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(50, 121);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(50, 15);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Usuario:";
+            this.lblUser.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(50, 154);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(70, 15);
+            this.lblPass.TabIndex = 1;
+            this.lblPass.Text = "Contraseña:";
+            this.lblPass.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(153, 118);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(125, 23);
+            this.txtUser.TabIndex = 3;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(153, 151);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(125, 23);
+            this.txtPass.TabIndex = 4;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(474, 309);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUser);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
     }
 }
 
--- GUI/LoginForm.cs
+++ GUI/LoginForm.cs
@@ -17,5 +17,14 @@ namespace Corvus_Proyecto
             InitializeComponent();
         }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
